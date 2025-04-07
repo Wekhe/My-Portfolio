57c320e43f95ab6ca076a30540fc173ccc3a4e38
Chizitere <ikembuchimwekhe@gmail.com>
2025-04-07 15:41:56 +0100

    Update index.js

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -46,7 +46,7 @@ const Portfolio = () => {
                 </h1>
                 <div>{renderPortfolio(portfolioData.portfolio)}</div>
             </div>
-            <Loader type="pacman"/>
+      <Loader type="semi-circle-spin" />
         </>
     );
 }
